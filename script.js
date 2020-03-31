211ac528329e003ae3a8636b748e9b91bd364373
William Antony <williamantony@ymail.com>
2020-03-31 23:24:29 +0700

    blabla

--- script.js
+++ script.js
@@ -163,5 +163,16 @@ $(document).ready(function(){
     }, 500);
     } 
     }
-    
-    });
+
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#scroll-top').fadeIn(); 
+        } else { 
+            $('#scroll-top').fadeOut(); 
+        } 
+    }); 
+    $('#scroll-top').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
+});
